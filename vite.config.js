19d7ab78371531320cec5e4cf12ca9785892cff6
Andrew Moreira <88143161+AndrewMoreira91@users.noreply.github.com>
2024-04-10 20:35:56 -0300

    Atualiza configurações de build no arquivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite'
+import { splitVendorChunkPlugin } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [splitVendorChunkPlugin(), react()],
   base: '/',
+  build: {
+    rollupOptions: {
+      input: {
+        main: './src/main.jsx',
+        pomodoro: './src/routes/pages/Pomodoro/Pomodoro.jsx',
+        dashboard: './src/routes/pages/DashBoard/Dashboard.jsx',
+        errorPage: './src/routes/Erro-page.jsx',
+      },
+      external: ['react', 'react-dom', 'react-router-dom', 'react-query'],
+    }
+  }
 })
