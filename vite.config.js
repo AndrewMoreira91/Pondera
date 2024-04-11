c27162ba05dc74506b31daa5e605d899cab16805
Andrew Moreira <88143161+AndrewMoreira91@users.noreply.github.com>
2024-04-10 21:11:53 -0300

    Atualiza configurações de build no arquivo vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/',
+  server: {
+    host: 'localhost',
+    hmr: {host: 'localhost'},
+  },
   build: {
     rollupOptions: {
       external: [
